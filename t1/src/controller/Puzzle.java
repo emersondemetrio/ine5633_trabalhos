55fc60b3ea2820114f895ed5a16f5c1c9465f5af
emerson demetrio <emer.demetrio@gmail.com>
2015-03-26 18:55:42 -0300

    arrumei o bug dos movimentos; arrumei o metodo getObjetivo(int item)

--- t1/src/controller/Puzzle.java
+++ t1/src/controller/Puzzle.java
@@ -1,7 +1,6 @@
 package controller;
 
 import model.Tabuleiro;
-import view.PuzzleGui;
 
 public class Puzzle {
 
@@ -9,7 +8,75 @@ public class Puzzle {
 
 	public Puzzle(int slots) {
 		this.tab = new Tabuleiro();
-		new PuzzleGui("8 Puzzle Game", tab);
+		// new PuzzleGui("Entre com um estado inicial", tab);
+
+		tab.setEstadoInicial(tab.getEstadoObjetivo());
+		Tabuleiro temp = tab;
+		int[][] teste = { { 1, 2, 3 }, { 4, 0, 6 }, { 7, 8, 5 } };
+		temp.setEstadoInicial(teste);
+		this.showStatus("Estado inicial", temp.getTabuleiro());
+
+		temp = tab.moveVazioParaDireita(tab);
+		if (temp != null) {
+			this.showStatus("Move para direita", tab.getTabuleiro());
+		} else {
+			System.out.println("Impossivel mover para direita.");
+		}
+
+		temp = tab.moveVazioParaEsquerda(tab);
+		if (temp != null) {
+			this.showStatus("Move para esquerda", temp.getTabuleiro());
+		} else {
+			System.out.println("Impossivel mover para esquerda.");
+		}
+
+		temp = tab.moveVazioParaCima(tab);
+		if (temp != null) {
+			this.showStatus("Move para cima", temp.getTabuleiro());
+		} else {
+			System.out.println("Impossivel mover para cima.");
+		}
+
+		temp = tab.moveVazioParaBaixo(tab);
+		if (temp != null) {
+			this.showStatus("Move baixo", temp.getTabuleiro());
+		} else {
+			System.out.println("Impossivel mover para baixo.");
+		}
+
+		temp = tab.moveVazioParaBaixo(tab);
+		if (temp != null) {
+			this.showStatus("Move baixo", temp.getTabuleiro());
+		} else {
+			System.out.println("Impossivel mover para baixo.");
+		}
+
+		temp = tab.moveVazioParaEsquerda(tab);
+		if (temp != null) {
+			this.showStatus("Move para esquerda", temp.getTabuleiro());
+		} else {
+			System.out.println("Impossivel mover para esquerda.");
+		}
+
+		temp = tab.moveVazioParaEsquerda(tab);
+		if (temp != null) {
+			this.showStatus("Move para esquerda", temp.getTabuleiro());
+		} else {
+			System.out.println("Impossivel mover para esquerda.");
+		}
+
+	}
+
+	public void showStatus(String msg, int[][] estado) {
+
+		String output = "";
+		for (int i = 0; i < estado.length; i++) {
+			for (int j = 0; j < estado.length; j++) {
+				output += " [" + estado[i][j] + "] ";
+			}
+			output += "\n";
+		}
+		System.out.println("\n  ---" + msg + "---\n" + output);
 	}
 
 }
--- t1/src/model/Tabuleiro.java
+++ t1/src/model/Tabuleiro.java
@@ -30,7 +30,6 @@ public class Tabuleiro {
 
 	protected int calcularDistanciaItemOrigem(int item, int linha, int coluna) {
 
-
 		int[] posObjetivo = this.getPosicalObjetivo(item);
 
 		int numeroPassosAteObjetivo = Math.abs((posObjetivo[0]) - (linha))
@@ -42,53 +41,6 @@ public class Tabuleiro {
 	public int[][] getEstadoObjetivo() {
 		int[][] objetivo = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
 		return objetivo;
-	}
-
-	public static void log(String s) {
-		System.out.println(s);
-	}
-
-	public int[] getPosicalObjetivo(int valor) {
-		int[] error = new int[] { -1, -1 };
-
-		int[] objetivo;
-		switch (valor) {
-		case 0:
-			objetivo = new int[] { 0, 0 };
-			break;
-		case 1:
-			objetivo = new int[] { 0, 1 };
-			break;
-		case 2:
-			objetivo = new int[] { 0, 2 };
-			break;
-
-		case 3:
-			objetivo = new int[] { 1, 0 };
-			break;
-		case 4:
-			objetivo = new int[] { 1, 1 };
-			break;
-		case 5:
-			objetivo = new int[] { 1, 2 };
-			break;
-		case 6:
-			objetivo = new int[] { 2, 0 };
-			break;
-		case 7:
-			objetivo = new int[] { 2, 1 };
-			break;
-		case 8:
-			objetivo = new int[] { 2, 2 };
-			break;
-
-		default:
-			objetivo = error;
-			break;
-		}
-
-		return objetivo;
-
 	}
 
 	public Tabuleiro moveVazioParaCima(Tabuleiro tabuleiro) {
@@ -103,9 +55,18 @@ public class Tabuleiro {
 			novoTab = new Tabuleiro();
 			novoTab.setEstadoInicial(this.getTabuleiro());
 			// guarda valor acima do vazio
-			int valorAcimaDoVazio = novoTab.getTabuleiro()[posicaoVazio[0] + 1][posicaoVazio[1]];
+			int linha = posicaoVazio[0];
+			int coluna = posicaoVazio[1];
+
+			int valorAcimaDoVazio = novoTab.getTabuleiro()[linha - 1][coluna];
 			// colocar o valor acima dentro da posicao vazia
-			novoTab.trocaPecas(posicaoVazio, valorAcimaDoVazio);
+
+			/*
+			 * @Change Antes o metodo trocaPecas recebia apenas posicaoVazio e o
+			 * valor. Eh necessario enviar tbm a direcao para que nao cause
+			 * nullPointerException na hora da troca.
+			 */
+			novoTab.trocaPecas(Movimentos.CIMA, posicaoVazio, valorAcimaDoVazio);
 
 		}
 		// senhao retorna tabuleiro invalido
@@ -116,58 +77,120 @@ public class Tabuleiro {
 	public Tabuleiro moveVazioParaBaixo(Tabuleiro tabuleiro) {
 		Tabuleiro novoTab = null;
 
-		int[] posicaoVazio = this.encontraPosicao(novoTab.tabuleiro, 0);
+		int[] posicaoVazio = this.encontraPosicao(this.tabuleiro, 0);
 
 		if (verificaSeMovimentoDoVazioEhValido(Movimentos.BAIXO, posicaoVazio)) {
 			novoTab = new Tabuleiro();
 			novoTab.setEstadoInicial(this.getTabuleiro());
-			int valorAbaixoDoVazio = novoTab.getTabuleiro()[posicaoVazio[0] - 1][posicaoVazio[1]];
-			novoTab.trocaPecas(posicaoVazio, valorAbaixoDoVazio);
+			int valorAbaixoDoVazio = novoTab.getTabuleiro()[posicaoVazio[0] + 1][posicaoVazio[1]];
+
+			novoTab.trocaPecas(Movimentos.BAIXO, posicaoVazio,
+					valorAbaixoDoVazio);
 		}
 
 		return novoTab;
 	}
 
 	public Tabuleiro moveVazioParaEsquerda(Tabuleiro tabuleiro) {
-		return null;
+		Tabuleiro novoTab = null;
+
+		int[] posicaoVazio = this.encontraPosicao(this.tabuleiro, 0);
+		if (verificaSeMovimentoDoVazioEhValido(Movimentos.ESQUERDA,
+				posicaoVazio)) {
+			novoTab = new Tabuleiro();
+			novoTab.setEstadoInicial(this.getTabuleiro());
+			int linha = posicaoVazio[0];
+			int coluna = posicaoVazio[1];
+
+			int valorAEsquerdaDoVazio = novoTab.getTabuleiro()[linha][coluna - 1];
+			novoTab.trocaPecas(Movimentos.ESQUERDA, posicaoVazio,
+					valorAEsquerdaDoVazio);
+		}
+
+		return novoTab;
 	}
 
-	public Tabuleiro moveVazioParaDireita(Tabuleiro tabuleiro){
-		return null;
+	public Tabuleiro moveVazioParaDireita(Tabuleiro tabuleiro) {
+		Tabuleiro novoTab = null;
+
+		int[] posicaoVazio = this.encontraPosicao(this.tabuleiro, 0);
+
+		boolean jogadaValida = verificaSeMovimentoDoVazioEhValido(
+				Movimentos.DIREITA, posicaoVazio);
+
+		if (jogadaValida) {
+			novoTab = new Tabuleiro();
+			novoTab.setEstadoInicial(this.getTabuleiro());
+			int linha = posicaoVazio[0];
+			int coluna = posicaoVazio[1];
+
+			int valorADireitaDoVazio = novoTab.getTabuleiro()[linha][coluna + 1];
+			
+			novoTab.trocaPecas(Movimentos.DIREITA, posicaoVazio,
+					valorADireitaDoVazio);
+		}
+
+		return novoTab;
 	}
 
-	private void trocaPecas(int[] posicaoVazio, int valorAcimaDoVazio) {
+	private void trocaPecas(int direcao, int[] posicaoVazio,
+			int valorSubstituto) {
+		switch (direcao) {
 
-		this.tabuleiro[posicaoVazio[0]][posicaoVazio[1]] = valorAcimaDoVazio;
-		// move o espa�o vazio para cima
-		this.tabuleiro[posicaoVazio[0] + 1][posicaoVazio[1]] = 0;
+		case Movimentos.CIMA:
+			this.tabuleiro[posicaoVazio[0]][posicaoVazio[1]] = valorSubstituto;
+			this.tabuleiro[posicaoVazio[0] - 1][posicaoVazio[1]] = 0;
+			break;
+
+		case Movimentos.BAIXO:
+			this.tabuleiro[posicaoVazio[0]][posicaoVazio[1]] = valorSubstituto;
+			this.tabuleiro[posicaoVazio[0] + 1][posicaoVazio[1]] = 0;
+			break;
+
+		case Movimentos.ESQUERDA:
+			this.tabuleiro[posicaoVazio[0]][posicaoVazio[1]] = valorSubstituto;
+			this.tabuleiro[posicaoVazio[0]][posicaoVazio[1] - 1] = 0;
+			break;
+
+		case Movimentos.DIREITA:
+			this.tabuleiro[posicaoVazio[0]][posicaoVazio[1]] = valorSubstituto;
+			this.tabuleiro[posicaoVazio[0]][posicaoVazio[1] + 1] = 0;
+			break;
+
+		default:
+			break;
+		}
 	}
 
 	public boolean verificaSeMovimentoDoVazioEhValido(int movimento,
 			int[] posicaoVazio) {
 
+		int linha = posicaoVazio[0];
+		int coluna = posicaoVazio[1];
+
 		switch (movimento) {
 		case Movimentos.CIMA:
-			return posicaoVazio[0] != 0;
+			return linha != 0;
+
 		case Movimentos.BAIXO:
-			return posicaoVazio[0] != 2;
+			return linha != 2;
+
 		case Movimentos.DIREITA:
-			return posicaoVazio[1] != 2;
+			return coluna != 2;
+
 		case Movimentos.ESQUERDA:
-			return posicaoVazio[1] != 0;
+			return coluna != 0;
 
 		default:
 			return false;
-
 		}
-
 	}
-
 
 	public int[] encontraPosicao(int[][] tabuleiro, int valor) {
 
 		for (int i = 0; i < tabuleiro.length; i++) {
 			for (int j = 0; j < tabuleiro[0].length; j++) {
+
 				if (tabuleiro[i][j] == valor) {
 					return new int[] { i, j };
 				}
@@ -176,4 +199,52 @@ public class Tabuleiro {
 		return new int[] { -1, -1 };
 	}
 
+	public int[] getPosicalObjetivo(int valor) {
+		int[] error = new int[] { -1, -1 };
+
+		int[] objetivo;
+		switch (valor) {
+
+		case 1:
+			objetivo = new int[] { 0, 0 };
+			break;
+		case 2:
+			objetivo = new int[] { 0, 1 };
+			break;
+		case 3:
+			objetivo = new int[] { 0, 2 };
+			break;
+
+		case 4:
+			objetivo = new int[] { 1, 0 };
+			break;
+		case 5:
+			objetivo = new int[] { 1, 1 };
+			break;
+		case 6:
+			objetivo = new int[] { 1, 2 };
+			break;
+
+		case 7:
+			objetivo = new int[] { 2, 0 };
+			break;
+		case 8:
+			objetivo = new int[] { 2, 1 };
+			break;
+		case 0:
+			objetivo = new int[] { 2, 2 };
+			break;
+
+		default:
+			objetivo = error;
+			break;
+		}
+
+		return objetivo;
+
+	}
+
+	static void debug(String s) {
+		System.out.println(s);
+	}
 }
