37f1efb88b45ddab042eee2f51eb3e47d4ec688b
jjduarte <duarte.jpdl@gmail.com>
2015-04-08 21:49:56 -0300

    Commit trabalho 2

---
+++ t2/src/model/Tabuleiro.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tabuleiro {
+
+	protected int[][] tabuleiro = new int[15][15];
+	protected List<int[]> posicoesOcupadas = new ArrayList<>();
+
+
+	public int[][] getVizinhos(int linha, int coluna ){
+		return null;
+	}
+}
