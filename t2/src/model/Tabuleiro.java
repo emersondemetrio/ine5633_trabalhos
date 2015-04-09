4a5d0b5193e77e468c8c8dc22324a59147fe4a5e
jjduarte <duarte.jpdl@gmail.com>
2015-04-09 00:13:24 -0300

    testes

--- t2/src/model/Tabuleiro.java
+++ t2/src/model/Tabuleiro.java
@@ -9,7 +9,30 @@ public class Tabuleiro {
 	protected List<int[]> posicoesOcupadas = new ArrayList<>();
 
 
+	public void setaPeao(int linha, int coluna, int jogador){
+		tabuleiro[linha][coluna] = jogador;
+	}
+
 	public int[][] getVizinhos(int linha, int coluna ){
 		return null;
 	}
+
+	public void showStatus(String msg) {
+
+		String output = "";
+		for (int i = 0; i < this.tabuleiro.length; i++) {
+			for (int j = 0; j < this.tabuleiro.length; j++) {
+				if(j == 0)
+					output += "Linha  "+i + "   ";
+				if(this.tabuleiro[i][j] == 0 ){
+					output += " | ";
+				}else{
+					output += " |" + this.tabuleiro[i][j] + "";
+				}
+			}
+			output += "\n";
+		}
+		System.out.println("\n  ---" + msg + "---\n" + output);
+	}
+
 }
