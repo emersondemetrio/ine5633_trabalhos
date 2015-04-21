be5405d5dbec2ecca97c0a3ec9fa49d810126fe0
Emerson Demetrio <emer.demetrio@gmail.com>
2015-04-21 19:09:07 -0300

    Novos atributos na classe Tabuleiro e seus respectivos métodos de acesso.

--- t2/src/model/Tabuleiro.java
+++ t2/src/model/Tabuleiro.java
@@ -7,8 +7,32 @@ public class Tabuleiro {
 
 	protected int[][] tabuleiro = new int[15][15];
 	protected List<int[]> posicoesOcupadas = new ArrayList<>();
+	protected float valor;
+	
+	public int[][] getTabuleiro() {
+		return tabuleiro;
+	}
 
+	public void setTabuleiro(int[][] tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
 
+	public List<int[]> getPosicoesOcupadas() {
+		return posicoesOcupadas;
+	}
+
+	public void setPosicoesOcupadas(List<int[]> posicoesOcupadas) {
+		this.posicoesOcupadas = posicoesOcupadas;
+	}
+
+	public float getValor() {
+		return valor;
+	}
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	
 	public void setaPeao(int linha, int coluna, int jogador){
 		tabuleiro[linha][coluna] = jogador;
 	}
